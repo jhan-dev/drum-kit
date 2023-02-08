69a7c9d940443d79710b54e675d7d41525175f0e
jhan-dev <jhan.technology@gmail.com>
2023-02-08 13:30:49 -0800

    Finished drum kit

--- index.js
+++ index.js
@@ -16,52 +16,125 @@ const drums = document.querySelectorAll(".drum")
 
 for (let i = 0; i < drums.length; i++) {
 
-    if (i === 0) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/kick-bass.mp3')
-        audio.play()
-        })
+    // Using If Else statements
+    // if (i === 0) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/kick-bass.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 1) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/hi-hat.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 2) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/snare.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 3) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/crash.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 4) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/tom-1.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 5) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/tom-2.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 6) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/tom-3.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // else if (i === 7) {
+    //     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    //     let audio = new Audio('sounds/tom-4.mp3')
+    //     audio.play()
+    //     })
+    // }
+    // ===========================================================
+
+    // Detect click press
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        let buttonInnerHTML = this.innerHTML
+        makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
+    })
+}
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key)
+    buttonAnimation(event.key)
+})
+
+// Detected keyboard press with switch expression
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            let kickbass = new Audio('sounds/kick-bass.mp3')
+            kickbass.play()
+            break
+
+        case "a":
+            let hihat = new Audio('sounds/hi-hat.mp3')
+            hihat.play()
+            break
+
+        case "s":
+            let snare = new Audio('sounds/snare.mp3')
+            snare.play()
+            break
+
+        case "d":
+            let crash = new Audio('sounds/crash.mp3')
+            crash.play()
+            break
+
+        case "i":
+            let tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play()
+            break
+
+        case "j":
+            let tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play()
+            break
+
+        case "k":
+            let tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play()
+            break
+
+        case "l":
+            let tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play()
+            break
+
+        default: console.log()
+
     }
-    else if (i === 1) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/hi-hat.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 2) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/snare.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 3) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/crash.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 4) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/tom-1.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 5) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/tom-2.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 6) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/tom-3.mp3')
-        audio.play()
-        })
-    }
-    else if (i === 7) {
-        document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        let audio = new Audio('sounds/tom-4.mp3')
-        audio.play()
-        })
-    }
+}
+
+function buttonAnimation(currentKey) {
+    let activeButton = document.querySelector(`.${currentKey}`)
+
+    activeButton.classList.add("pressed")
+    
+    setTimeout(function() {
+        activeButton.classList.remove("pressed"); 
+    }, 100)
 }
